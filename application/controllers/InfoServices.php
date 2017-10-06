34eb64149f90eb60b4b0c88bcb70a7c27a96c567
SungTaoKao <paulkao94@gmail.com>
2017-10-06 16:04:36 -0700

    added infoService Controller

---
+++ application/controllers/InfoServices.php
@@ -0,0 +1,30 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class InfoServices extends Application
+{
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/
+	 * 	- or -
+	 * 		http://example.com/welcome/index
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$this->data['pagebody'] = 'welcome_message';
+		$this->render(); 
+	}
+    
+    public function info(){
+        
+    }
+
+}
