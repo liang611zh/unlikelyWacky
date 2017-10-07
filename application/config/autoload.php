de315b32785c9729fca840a58b8f73645579abfa
Liang Zhao <zhaoliangbz@gmail.com>
2017-10-07 15:24:01 -0700

    feature-flights closes #4

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('fleet', 'flights');
+$autoload['model'] = array('fleet', 'flight');
