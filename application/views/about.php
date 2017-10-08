db941992114f68272888ca651380637e949445b9
Kabir Cahill <kcahill4@my.bcit.ca>
2017-10-07 18:20:33 -0700

    Added About controller and view.

---
+++ application/views/about.php
@@ -0,0 +1,3 @@
+<h1>About</h1>
+        <p>Unlikely Wacky Airline is a fictional airline website created by computing students for a class project.</p>
+        <p>This website is not related to, nor endorsed by any similar business from the real world.</p>
