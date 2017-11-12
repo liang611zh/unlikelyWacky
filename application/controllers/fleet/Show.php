25b3808d9cf1b32bf33b0cdd1e28c5326f109651
Kabir Cahill <kcahill4@my.bcit.ca>
2017-11-11 23:28:04 -0800

    Added edit fleet page. Form submission still needs to be implemented.

--- application/controllers/fleet/Show.php
+++ application/controllers/fleet/Show.php
@@ -11,6 +11,10 @@ class Show extends Application
 	 */
 	public function index($key)
 	{
+        if ($this->session->userdata('userrole') == ROLE_OWNER) {
+            redirect('/fleet/edit/' . $key);
+        }
+
 		/*data of selected plane.*/
 		$plane = $this->fleet->get($key);
 
@@ -48,8 +52,12 @@ class Show extends Application
      */
     public function edit($id = null)
     {
-        if ($id == null || $this->session->userdata('userrole') != ROLE_OWNER) {
+        if ($id == null) {
             redirect('/fleet');
+        }
+
+        if ($this->session->userdata('userrole') != ROLE_OWNER) {
+            redirect('/fleet/' . $id);
         }
 
         /*data of selected plane.*/
@@ -58,27 +66,69 @@ class Show extends Application
         /*set pagetitle to the id of the plane.*/
         $this->data['pagetitle'] = $plane->id;
 
-        $this->data['pagebody'] = 'fleet/plane';
+        $this->session->set_userdata('plane', $plane);
 
-        /*table opening*/
-        $table_open = '<table class="table table-striped table-hover">';
+        $this->showit();
+    }
 
-        /*table content ;a row shown as :   propertyName : propertyValue*/
-        $table_content = "";
+    // Render the current DTO
+    private function showit()
+    {
+        $this->load->helper('form');
+        $plane = $this->session->userdata('plane');
+        $this->data['id'] = $plane->id;
 
-        foreach($plane as $key => $val) {
-            $table_content .= "<tr>
-          							<th>$key</th>
-          							<td>$val</td>
-        						</tr>";
-        }
+        // if no errors, pass an empty message
+        if ( ! isset($this->data['error']))
+            $this->data['error'] = '';
 
-        /*table closing*/
-        $table_close = '</table>';
+        $fields = array(
+            'fmanufacturer'  => form_label('Manufacturer') . form_input('manufacturer', $plane->manufacturer),
+            'fmodel'      => form_label('Model') . form_input('size', $plane->model),
+            'fprice'     => form_label('Price') . form_input('price', $plane->price),
+            'fseats'     => form_label('Seats') . form_input('seats', $plane->seats),
+            'freach'     => form_label('Reach') . form_input('reach', $plane->reach),
+            'fcruise'     => form_label('Cruise') . form_input('cruise', $plane->cruise),
+            'ftakeoff'     => form_label('Takeoff') . form_input('takeoff', $plane->takeoff),
+            'fhourly'     => form_label('Hourly') . form_input('hourly', $plane->hourly),
+            'zsubmit'    => form_submit('submit', 'Update the plane'),
+        );
+        $this->data = array_merge($this->data, $fields);
 
-        /*the table to display in the fleet/plane view*/
-        $this->data['singlePlaneTable'] = $table_open .$table_content. $table_close;
-
+        $this->data['pagebody'] = '/fleet/planeedit';
         $this->render();
+    }
+
+    // handle form submission
+    public function submit()
+    {
+        // setup for validation
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules($this->fleet->rules());
+
+        // retrieve & update data transfer buffer
+        $plane = (array) $this->session->userdata('plane');
+        $plane = array_merge($plane, $this->input->post());
+        $plane = (object) $plane;  // convert back to object
+        $this->session->set_userdata('plane', (object) $plane);
+
+        // validate away
+        if ($this->form_validation->run())
+        {
+            if (empty($plane->id))
+            {
+                $plane->id = $this->tasks->highest() + 1;
+                $this->tasks->add($plane);
+                $this->alert('Plane ' . $plane->id . ' added', 'success');
+            } else
+            {
+                $this->tasks->update($plane);
+                $this->alert('Plane ' . $plane->id . ' updated', 'success');
+            }
+        } else
+        {
+            $this->alert('<strong>Validation errors!<strong><br>' . validation_errors(), 'danger');
+        }
+        $this->showit();
     }
 }
