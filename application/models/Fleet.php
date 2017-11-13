63df87951dc139d8b69733cc8f3baf04734ab15e
Kent Huang <kenttttt0@gmail.com>
2017-11-12 20:08:38 -0800

    added fleet rules

--- application/models/Fleet.php
+++ application/models/Fleet.php
@@ -21,19 +21,58 @@ class Fleet extends CSV_Model
 	}
 
     // provide form validation rules
+
     public function rules()
     {
         $config = array(
-            ['field' => 'id', 'label' => 'Plane id', 'rules' => 'alpha_numeric_spaces|max_length[4]|required'],
-            // ['field' => 'manufacturer', 'label' => 'Manufacturer', 'rules' => 'alpha_numeric_spaces|max_length[64]'],
-            // ['field' => 'model', 'label' => 'Model', 'rules' => 'alpha_numeric_spaces|max_length[64]'],
-            // ['field' => 'price', 'label' => 'Price', 'rules' => 'numeric|greater_than[0]'],
-            // ['field' => 'seats', 'label' => 'Number of seats', 'rules' => 'integer|greater_than[0]'],
-            // ['field' => 'reach', 'label' => 'Reach', 'rules' => 'integer|greater_than[0]'],
-            // ['field' => 'cruise', 'label' => 'Cruise', 'rules' => 'integer|greater_than[0]'],
-            // ['field' => 'takeoff', 'label' => 'Takeoff', 'rules' => 'integer|greater_than[0]'],
-            // ['field' => 'hourly', 'label' => 'Hourly', 'rules' => 'integer|greater_than[0]'],
+            ['field' => 'id', 'label' => 'id', 'rules' => array(
+                'required', 
+                //here specify the custom rule
+                //second field tells where to find the validation funcs
+                array('id', array($this->fleet, 'id_check'))
+                )
+             ],
+             ['field' => 'wackyid', 'label' => 'recognizedPlane', 'rules' => array( 
+                //here specify the custom rule
+                //second field tells where to find the validation funcs
+                array('wackyselect', array($this->fleet, 'recognizedPlane_check'))
+                )
+             ],
         );
         return $config;
+    }
+
+
+    //here to check the plane id
+    public function id_check($id) {
+        if ($id == 'wrong')
+        {
+                $this->form_validation->set_message('id', 'The {field} field should start with U and must be unique.');
+                return FALSE;
+        }
+        else
+        {
+                //echo "checking";
+                return TRUE;
+        }
+    }
+
+
+
+    //here to check the wacky id
+    public function recognizedPlane_check($wackyid) {
+
+        $allid = $this->airplanesWacky->getAllid();
+        var_dump($wackyid);
+        var_dump($allid);
+
+        foreach ($allid as $value) {
+            if($wackyid == $value) {
+                return TRUE;
+            }
+        }
+       $this->form_validation->set_message('wackyselect', 'The {field} field is not valid');
+                
+        return FALSE;
     }
 }
