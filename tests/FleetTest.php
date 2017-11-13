0f7a6938bbb0b5932e3bfc01b9078d01b21c3017
SungTaoKao <paulkao94@gmail.com>
2017-11-12 21:40:57 -0800

    Finished Test Cases

--- tests/FleetTest.php
+++ tests/FleetTest.php
@@ -12,18 +12,32 @@ class FleetTest extends PHPUnit_Framework_TestCase
     private $fleet;
     
     public function setUp(){
-        $this->fleet = new Fleet();        
+        $this->fleet = new Fleet();
+        $this->fleet->load->library('form_validation');
     }
     
     //-----test id_check-----//
     public function testId_checkBasic(){
         $this->assertEquals(true, $this->fleet->id_check("U1"));
     }
-    public function testNotWrong(){
-        $this->assertEquals(true, $this->fleet->id_check(" "));
+    public function testEmptyString(){
+        $this->assertEquals(false, $this->fleet->id_check(""));
+    }
+    public function testNotStartWithU(){
+        $this->assertEquals(false, $this->fleet->id_check("V"));
+    }
+    public function testLengthGTThree(){
+        $this->assertEquals(false, $this->fleet->id_check("U234"));
     }
     public function testIs_wrong(){
         $this->assertEquals(false, $this->fleet->id_check("wrong"));
     }
     
+    //-----test recognizedPlane_check-----//
+    public function testRecognizedPlane_CheckBasic(){
+        $this->assertEquals(true, $this->fleet->recognizedPlane_check("baron"));
+    }
+    public function testInvalidPlane(){
+        $this->assertEquals(false, $this->fleet->recognizedPlane_check("invalid"));
+    }
 }
